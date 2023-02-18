d4577951af2fb02b952b986a4e1fdfd1c32dba16
Marks Ruziboev <112407232+marksruziboev@users.noreply.github.com>
2023-02-18 14:03:46 +0100

    Update adding_books_to_shelf_x.c
    corrected 2d malloc

--- adding_books_to_shelf_x.c
+++ adding_books_to_shelf_x.c
@@ -14,57 +14,59 @@ int** total_number_of_pages;
 
 int main()
 {
-    int total_number_of_shelves;
+    int i,total_number_of_shelves;
     scanf("%d", &total_number_of_shelves);
     
     int total_number_of_queries;
     scanf("%d", &total_number_of_queries);
-    
-    total_number_of_books = (int*) calloc(total_number_of_shelves, sizeof(int));
-    total_number_of_pages = (int**) calloc(total_number_of_shelves, sizeof(int*));
+    total_number_of_books = (int *)malloc(sizeof(int) * total_number_of_shelves);
+
+   total_number_of_pages = (int**)malloc(sizeof(int*) *total_number_of_shelves);
+   for (i = 0; i < total_number_of_shelves; i++)
+   {     total_number_of_books[i]=0;
+       total_number_of_pages[i] = (int*) malloc(sizeof(int));
+       }
     
     while (total_number_of_queries--) {
         int type_of_query;
         scanf("%d", &type_of_query);
         
         if (type_of_query == 1) {
+            /*
+             * Process the query of first type here.
+             */
             int x, y;
             scanf("%d %d", &x, &y);
-            int shelf_index = x - 1;
-            int book_index = total_number_of_books[shelf_index];
+            total_number_of_books[x] += 1;
+            total_number_of_pages[x] = realloc(*(total_number_of_pages+x), *(total_number_of_books+x)*sizeof(int));
+            //*(*(total_number_of_pages+x)+*(total_number_of_books+x)-1)=y;
+            total_number_of_pages[x][total_number_of_books[x]-1] = y;
             
-            if (!total_number_of_pages[shelf_index]) {
-                total_number_of_pages[shelf_index] = (int*) calloc(1100, sizeof(int));
-            }
-            
-            total_number_of_pages[shelf_index][book_index] = y;
-            total_number_of_books[shelf_index]++;
-            printf("%d\n", total_number_of_pages[shelf_index][book_index] );
-            printf("%d\n", x);
+
         } else if (type_of_query == 2) {
             int x, y;
             scanf("%d %d", &x, &y);
-            int shelf_index = x - 1;
-            int book_index = y - 1;
-            
-            printf("%d\n", total_number_of_pages[shelf_index][book_index]);
+            printf("%d\n", *(*(total_number_of_pages + x) + y));
         } else {
             int x;
             scanf("%d", &x);
-            int shelf_index = x - 1;
-            
-            printf("%d\n", total_number_of_books[shelf_index]);
+            printf("%d\n", *(total_number_of_books + x));
+        }
+    }
+
+    if (total_number_of_books) {
+        free(total_number_of_books);
+    }
+    
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        if (*(total_number_of_pages + i)) {
+            free(*(total_number_of_pages + i));
         }
     }
     
-    free(total_number_of_books);
-    for (int i = 0; i < total_number_of_shelves; i++) {
-        if (total_number_of_pages[i]) {
-            free(total_number_of_pages[i]);
-        }
+    if (total_number_of_pages) {
+        free(total_number_of_pages);
     }
-    free(total_number_of_pages);
     
     return 0;
 }
-
